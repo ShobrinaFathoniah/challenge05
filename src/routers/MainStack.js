31f0ecb31bd779a2f21c6db630c26b4f47821958
ShobrinaFathoniah <shobrinaf@gmail.com>
2022-05-09 19:12:12 +0700

    feat: add navigation + git action

---
+++ src/routers/MainStack.js
@@ -0,0 +1,58 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React from 'react';
+import {Home, Login, Maps, Register} from '../screens';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainStack = () => {
+  return (
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen
+        name="Register"
+        component={Register}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="MainApp"
+        component={MainApp}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const MainApp = () => (
+  <Tab.Navigator screenOptions={{headerShown: false}}>
+    <Tab.Screen
+      options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: ({color, size}) => (
+          <AntDesign name="home" color={color} size={size} />
+        ),
+      }}
+      name="Home"
+      component={Home}
+    />
+    <Tab.Screen
+      options={{
+        tabBarLabel: 'Maps',
+        tabBarIcon: ({color, size}) => (
+          <Fontisto name="map" color={color} size={size} />
+        ),
+      }}
+      name="Maps"
+      component={Maps}
+    />
+  </Tab.Navigator>
+);
+
+export default MainStack;
---
+++ src/screens/index.js
@@ -0,0 +1,6 @@
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
+import Maps from './Maps';
+
+export {Home, Login, Register, Maps};
